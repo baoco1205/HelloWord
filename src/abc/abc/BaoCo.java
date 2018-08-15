a80cb549f7764439fbd06905c0c1c00af5d4178b
Baoco15254516894 <co.letruongbao@students.hueic.edu.vn>
2018-08-15 16:56:57 +0700

    tonghieutichthuong

--- src/abc/abc/BaoCo.java
+++ src/abc/abc/BaoCo.java
@@ -3,6 +3,15 @@ package abc.abc;
 public class BaoCo {
 	public static void main(String[] args) {
 	System.out.println("HelloWord");	
+	int a=10;
+	int b=2;
+	int tong=a+b;
+	int hieu=a-b;
+	int tich=a*b;
+	float thuong=a/b;
+	System.out.println(tong);
+	System.out.println(hieu);
+	System.out.println(tich);
+	System.out.println(thuong);
 	}
-
 }
